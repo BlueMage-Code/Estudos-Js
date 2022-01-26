9c0d1865c52872e515d1bcda96cfebd11f1a58d2
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2022-01-26 06:47:10 -0300

    Conversor de temperatura

---
+++ Kelvin Weather/script.js
@@ -0,0 +1,6 @@
+const Kelvin = 0
+let Celsius = Kelvin - 273
+let Fahrenheit = Math.floor(Celsius * (9/5) + 32)
+console.log(`The temperatura is ${Fahrenheit} degrees Fahrenheit`)
+let Newton = Celsius * (33/100)
+console.log(Newton)
