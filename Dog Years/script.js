c7d9b73afa5a7830021d8de0488a2ec7f46f3ccf
Roberto Araujo <80107325+BlueMage-Code@users.noreply.github.com>
2022-01-26 06:50:44 -0300

    Idade de Cachorro

---
+++ Dog Years/script.js
@@ -0,0 +1,8 @@
+let myAge = 22
+let earlyYears = 2
+const myName = 'Paul'.toLowerCase()
+earlyYears *= 10.5
+laterYears = myAge - 2 
+laterYears *= 4
+let myAgeInDogYears = earlyYears + laterYears
+console.log(`My name is ${myName}. I am ${myAge} years old is human years which is ${myAgeInDogYears} years old in do years.`)
